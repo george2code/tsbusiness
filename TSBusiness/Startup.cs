b736d8453c0cfec4c0fcd3c67672eac199f207ed
Yuriy Alexandrov <george2code@gmail.com>
2014-11-10 17:23:24 +0200

    auth (login, register...) owin

---
+++ TSBusiness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TSBusiness.Startup))]
+namespace TSBusiness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
