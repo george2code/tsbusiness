76bc09e523a1da93650c1dcb7bfdb13eb76e4993
Yuriy Alexandrov <george2code@gmail.com>
2014-11-13 18:57:51 +0200

    add tinyMCE editor

---
+++ TSBusiness/Models/TemplateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSBusiness.Models
+{
+    public class TemplateViewModel
+    {
+        [Required]
+        public int Delay { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [UIHint("tinymce_jquery_full"), AllowHtml]
+        public string Content { get; set; }
+    }
+}
